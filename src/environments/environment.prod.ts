e3ba7220d823aac8d7aec42deb4fb119c7878f9c
Guillermo Andrada <guillermoandrada@gmail.com>
2022-05-24 18:44:13 -0300

    nuevas configs

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebaseConfig: {
+    apiKey: 'AIzaSyDVxx1K4KqglV0EP-JzRV34pQoTWHv59YQ',
+    authDomain: 'argentina-programa-123.firebaseapp.com',
+    projectId: 'argentina-programa-123',
+    storageBucket: 'argentina-programa-123.appspot.com',
+    messagingSenderId: '241370394506',
+    appId: '1:241370394506:web:ee65f6433e5fe6f680a273',
+  },
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,15 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebaseConfig: {
+    apiKey: 'AIzaSyDVxx1K4KqglV0EP-JzRV34pQoTWHv59YQ',
+    authDomain: 'argentina-programa-123.firebaseapp.com',
+    projectId: 'argentina-programa-123',
+    storageBucket: 'argentina-programa-123.appspot.com',
+    messagingSenderId: '241370394506',
+    appId: '1:241370394506:web:ee65f6433e5fe6f680a273',
+  },
 };
 
 /*
